089e8e42228b120a66178230cbdd901e7f84139b
Tawfiq JAFFAR <tawfiq.jaffar@jump-informatique.com>
2018-09-04 11:30:21 +0200

    done first app. moving on to calculator

--- myfirstapp/app/src/main/java/com/example/tjaffar/myfirstapp/MainActivity.kt
+++ myfirstapp/app/src/main/java/com/example/tjaffar/myfirstapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.tjaffar.myfirstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.tjaffar.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun compute(view: View) {
+        val num1 = findViewById<EditText>(R.id.editTextNum1)
+        val num2 = findViewById<EditText>(R.id.editTextNum2)
     }
 }
